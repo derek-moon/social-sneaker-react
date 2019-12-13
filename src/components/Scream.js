075deb8a000efe063456fd470f5a70226752eb93
mxxnus <lxnkzr@gmail.com>
2019-12-12 19:09:23 -0500

    added styling to image cards

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import Typography from '@material-ui/core/Typography'
+import Link from 'react-router-dom/Link'
 
 //MUI STUFF 
 import Card from '@material-ui/core/Card';
@@ -9,8 +10,17 @@ import CardContent from '@material-ui/core/CardContent';
 
 const styles = {
     card:{
-        display:'flex'
+        display:'flex',
+        marginBottom:20,
+    },
+    image:{
+        minWidth:200
+    },
+    content:{
+        padding:25,
+        objectFit:'cover'
     }
+
 }
 
 class Scream extends Component {
@@ -19,12 +29,14 @@ class Scream extends Component {
         //destructure the props
         const {classes, scream : { body,createdAt,userImage,userHandle,screamId,likeCount,commentCount} } = this.props
         return (
-            <Card>
+            <Card className={classes.card}>
                 <CardMedia 
                 image={userImage}
-                title="Profile image"/>
-                <CardContent>
-                    <Typography variant="h5">{userHandle}</Typography>
+                title="Profile image"
+                className={classes.image}
+                />
+                <CardContent class={classes.content}>
+                    <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
                     <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>

c8d67e4d1ee9e31315ad7f892a017c8af62d1f44
mxxnus <lxnkzr@gmail.com>
2019-12-14 13:01:57 -0500

    added errors in helper text

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,87 +1,134 @@
-import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles'
-import PropTypes from 'prop-types';
-import AppIcon from '../images/icon.png'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/icon.png";
+import axios from "axios";
 
 //MUI STUFF
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 const styles = {
-    form:{
-        textAlign: 'center '
-    },
-    image:{
-        margin:'20px auto 20px auto'
-    },
-    pageTitle:{
-        margin: '10px auto 10px auto'
-    },
-    textField:{
-        margin:'10px auto 10px auto'
-    },
-    button:{
-        marginTop:20,
-    }
-
+  form: {
+    textAlign: "center "
+  },
+  image: {
+    margin: "20px auto 20px auto"
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto"
+  },
+  textField: {
+    margin: "10px auto 10px auto"
+  },
+  button: {
+    marginTop: 20
+  }
 };
 
-
-
 class login extends Component {
-    //controlled component w/ state
-    constructor(){
-        super();
-        this.state = {
-            email:'',
-            password:'',
-            loading: false,
-            errors:{}
-        }
-    }
+  //controlled component w/ state
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {}
+    };
+  }
 
-    handleSubmit = (event) => {
-        console.log('hi')
-    }
-
-    handleChange = (event) => {
+  handleSubmit = event => {
+    event.preventDefault();
+    //want to send request to server and show errors or redirect
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post("/login", userData)
+      .then(res => {
+        console.log(res.data);
         this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+          loading: false
+        });
+        //push path and then g to it
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        this.setState({
+          //error object
+          errors: err.response.data,
+          loading: false
+        });
+      });
+  };
 
-    render() {
-        const {classes} = this.props;
-        return (
-           <Grid container className={classes.form}>
-               <Grid item sm/>
-               <Grid item sm> 
-                    <img src={AppIcon} alt="monkey image" className={classes.image}/>
-                    <Typography variant="h2" className={classes.pageTitle}>
-                        Login
-                    </Typography>
-                    <form noValidate onSubmit={this.handleSubmit}>
-                        <TextField id="email" name="email" type="email" label="Email" className={classes.textField}
-                        value={this.state.email} onChange={this.handleChange} fullWidth/>
-                        <TextField id="password" name="password" type="password" label="Password" className={classes.textField}
-                        value={this.state.password} onChange={this.handleChange} fullWidth/>
-                        <Button type="submit" variant="contained" color="primary" className={classes.button}>
-                            Login
-                        </Button>
-                    </form>
-               </Grid>
-                
-               <Grid item sm/>
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-           </Grid>
-            
-        )
-    }
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="monkey image" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login
+            </Button>
+          </form>
+        </Grid>
+
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(login);

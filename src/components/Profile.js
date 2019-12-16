8aea16e34ec9b2d86322b190c42d63f252878c30
mxxnus <lxnkzr@gmail.com>
2019-12-16 15:27:37 -0500

    user able to submit new details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
+import EditDetails from "./EditDetails";
 //MUI
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
@@ -21,6 +22,7 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
 import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from "@material-ui/icons/KeyboardReturn";
 
 const styles = theme => ({
   ...theme.spreadIt
@@ -36,6 +38,9 @@ class Profile extends Component {
   handleEditPicture = () => {
     const fileInput = document.getElementById("imageInput");
     fileInput.click();
+  };
+  handleLogout = () => {
+    this.props.logoutUser();
   };
   render() {
     const {
@@ -98,6 +103,12 @@ class Profile extends Component {
               <CalendarToday color="primary" />{" "}
               <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
             </div>
+            <Tooltip title="logout" placement="top">
+              <IconButton onClick={this.handleLogout}>
+                <KeyboardReturn color="primary" />
+              </IconButton>
+            </Tooltip>
+            <EditDetails />
           </div>
         </Paper>
       ) : (
